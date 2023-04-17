74a8270662f67a6614a405c9e7a8561880b25d76
Ali Nasir <alee.nasir254@gmail.com>
2023-04-17 15:22:04 +0500

    testimonials & contact page completed

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,23 @@
 import "./contact.scss";
 
 export default function Contact() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
     <div className="contact" id="contact">
-      Contact
+      <div className="left">
+        <img src="assets/shake.svg" alt="" />
+      </div>
+      <div className="right">
+        <h2>Contact</h2>
+        <form onSubmit={handleSubmit}>
+          <input type="text" required placeholder="Full Name" />
+          <input type="email" required placeholder="Email" />
+          <textarea required placeholder="Message:"></textarea>
+          <button type="submit">Send</button>
+        </form>
+      </div>
     </div>
   );
 }
